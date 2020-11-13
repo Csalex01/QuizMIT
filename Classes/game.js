3414e57036fb25ded75ee6a1b118b7b009326741
Csillag Alex <csillagalex2001@gmail.com>
2020-11-13 23:33:29 +0200

    Added questions + moved the game logic to Game class

--- Classes/game.js
+++ Classes/game.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Import neccessary modules
+const Discord = require('discord.js')
 const Question = require("./question")
 // Required for file management
 const fs = require("fs")
@@ -48,6 +49,59 @@ class GameClass {
 
         // DEBUG MESSAGE (REMOVE LATER)
         console.log(`PlayerID: ${this.player.id}`);
+    }
+
+    handleQuestion = async (message, reaction) => {
+        // If there is an incoming answer
+        // FIX: Make this not emoji-oriented!
+        if (this.acceptedAnswers.includes(reaction.emoji.name)) {
+            if (reaction.emoji.name == "0️⃣" && this.currentQuestion.correctAnswer == 0)
+                this.player.correctAnswers += 1
+            else if (reaction.emoji.name == "1️⃣" && this.currentQuestion.correctAnswer == 1)
+                this.player.correctAnswers += 1
+            else if (reaction.emoji.name == "2️⃣" && this.currentQuestion.correctAnswer == 2)
+                this.player.correctAnswers += 1
+            else if (reaction.emoji.name == "3️⃣" && this.currentQuestion.correctAnswer == 3)
+                this.player.correctAnswers += 1
+            else
+                console.log("WRONG ANSWER!")
+
+            this.nextQuestion()
+        }
+
+        // While (if) the current this is running (+ off by 1 error correction)
+        if (this.currentQuestionNumber <= this.questionCount - 1) {
+            // Create a MessageEmbed object as response
+            let embed = new Discord.MessageEmbed()
+            let embedContent = ``
+
+            // Set the properties of the embed message
+            embed.setTitle(`${this.currentQuestionNumber}. ${this.currentQuestion.question}`)
+            embedContent += `Eddigi pontszámod: ${this.player.correctAnswers}/${this.questionCount}\n\n`
+            embedContent += `0️⃣ ${this.currentQuestion.answer0}\n`
+            embedContent += `1️⃣ ${this.currentQuestion.answer1}\n`
+            embedContent += `2️⃣ ${this.currentQuestion.answer2}\n`
+            embedContent += `3️⃣ ${this.currentQuestion.answer3}\n`
+            embed.setColor("GREEN")
+            embed.setDescription(embedContent)
+            embed.setThumbnail(this.currentQuestion.imgURL)
+
+            // Send the embed message
+            let botReply = await message.channel.send(embed)
+
+            // Add reactions to the embed message
+            for (let reaction of this.acceptedAnswers)
+                await botReply.react(reaction)
+
+            // DEBUG LOG
+            console.log(this.currentQuestion.correctAnswer)
+        } else {
+            // If the game is over
+            // DEBUG MESSAGE (REMOVE LATER)
+            console.log("GAME OVER")
+            message.channel.send(`JÁTÉK VÉGE\nElért pontszám: ${this.player.correctAnswers}\\${this.questionCount}`)
+            this.reset()
+        }
     }
 
     // Switch to next question
